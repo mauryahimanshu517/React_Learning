aaa2965d383dc19e6406b8dcfa014f5bbc033e89
mauryahimanshu517 <mauryahimanshu517@gmail.com>
2025-10-02 02:18:37 +0530

    vowel and consonant

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-let inputBaseNumber=2
-let inputPowerNumber=3
+// let inputBaseNumber=2
+// let inputPowerNumber=3
 
 
-function exponent(inputBaseNumber,inputPowerNumber){
-    let result=1
-    for(let i=1;i<=inputPowerNumber;i++){
-        result=result*inputBaseNumber
+// function exponent(inputBaseNumber,inputPowerNumber){
+//     let result=1
+//     for(let i=1;i<=inputPowerNumber;i++){
+//         result=result*inputBaseNumber
+//     }
+//     console.log(result)
+// }
+
+// exponent(inputBaseNumber,inputPowerNumber)
+
+//Voewl and consonent
+
+let str="hello world"
+
+let x=[...str]
+let count=0
+let constant=0
+for(let i=0;i<str.length;i++){
+    if("aeiou".includes(x[i])){
+        count=count+1
     }
-    console.log(result)
+    else{
+        constant=constant+1
+    }
+    
 }
+console.log(count)
+console.log(constant)
 
-exponent(inputBaseNumber,inputPowerNumber)
+
+
+
