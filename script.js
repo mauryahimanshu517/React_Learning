b70e4785104ab7e95510e26ab520edacf10d08be
mauryahimanshu517 <mauryahimanshu517@gmail.com>
2025-10-03 02:12:18 +0530

    introduct new feature

--- script.js
+++ script.js
@@ -14,22 +14,32 @@
 
 //Voewl and consonent
 
-let str="hello world"
+// let str="hello world"
 
-let x=[...str]
-let count=0
-let constant=0
-for(let i=0;i<str.length;i++){
-    if("aeiou".includes(x[i])){
-        count=count+1
-    }
-    else{
-        constant=constant+1
-    }
+// let x=[...str]
+// let count=0
+// let constant=0
+// for(let i=0;i<str.length;i++){
+//     if("aeiou".includes(x[i])){
+//         count=count+1
+//     }
+//     else{
+//         constant=constant+1
+//     }
     
-}
-console.log(count)
-console.log(constant)
+// }
+// console.log(count)
+// console.log(constant)
+
+let str = "heloo I'm from India";
+
+
+console.log(str.split(" ").slice(0,2).join(" "))
+
+
+
+
+
 
 
 
